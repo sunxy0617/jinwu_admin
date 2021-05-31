f9759760b65cefc04a802549a182e775fa940f16
孙新宇 <602184489@qq.com>
2021-05-31 11:12:50 +0800

    配置乐观锁

--- src/main/java/cn/nayuguo/jw/common/bean/MybatisPlus.java
+++ src/main/java/cn/nayuguo/jw/common/bean/MybatisPlus.java
@@ -1,5 +1,19 @@
 package cn.nayuguo.jw.common.bean;
 
-public class MybatisPlus {
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.OptimisticLockerInnerInterceptor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+/**
+ * @author Tuma_Coder
+ */
+@Configuration
+public class MybatisPlus {
+    @Bean
+    public MybatisPlusInterceptor mybatisPlusInterceptor() {
+        MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
+        interceptor.addInnerInterceptor(new OptimisticLockerInnerInterceptor());
+        return interceptor;
+    }
 }
